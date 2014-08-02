eceb5352de522efdb1c418bf1208ebb41b78bf05
Bloodyaugust <greysonrichey@gmail.com>
2014-08-01 21:23:22 -0600

    Finished hooking up SoundCloud API, visualizers now use real input, added a few helper functions for visualizers, re-did some UI elements and colors.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,42 @@
 (function ($) {
-
-    var stats = new Stats();
-    stats.setMode(1); // 0: fps, 1: ms
-
-    // Align top-left
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-
-    $('body').append(stats.domElement);
-
     var visualizer = $('canvas').trivisualizer({
-        frequencyBuffer: [100, 20, 33, 200]
+        activeVisualizer: 'blueDream'
     });
     visualizer.start();
+
+    SC.initialize({
+        client_id: "6015fc7ae3911c0283de765d4d138291",
+        redirect_uri: "http://0.0.0.0:9000/callback.html"
+    });
+
+    var user, playlists, favorites;
+
+    $('.soundcloud').click(function () {
+        SC.connect(function() {
+          SC.get('/me', function(me) {
+            user = me;
+            $('.soundcloud').hide();
+            $('.media-control-header .avatar').append('<img src="' + me['avatar_url'] + '" class="img-responsive">');
+            $('.media-control-header .username').append('<h3>' + me.username + '</h3>');
+        });
+          SC.get('/me/playlists', function(userPlaylists) {
+            playlists = userPlaylists;
+        });
+          SC.get('/me/favorites', function(userFavorites) {
+            favorites = userFavorites;
+            $.each(favorites, function (i, val) {
+                var newTrack = $('<span class="col-lg-12">' + val.title + '</span>').appendTo('.media-control-playlist');
+
+                newTrack.data('trackid', val.id);
+                newTrack.click(function () {
+                    SC.stream('/track/' + $(this).data()['trackid'], function (sound) {
+                        var audio = $('.trivisualizer-audio');
+
+                        audio.attr('src', sound.url.replace('track', 'tracks'));
+                    });
+                });
+            });
+        });
+      });
+    });
 }(jQuery));
--- app/scripts/trivisualizer.js
+++ app/scripts/trivisualizer.js
@@ -18,14 +18,71 @@
     }
     // the above from http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 
+    function bufferToArray(buffer) {
+        var newArray = [],
+            i;
+
+        for (i = 0; i < buffer.length; i++) {
+            newArray.push(buffer[i]);
+        }
+        return newArray;
+    }
+
+    function audioClipEmpty(buffer) {
+        var newArray = [],
+            i;
+
+        for (i = 0; i < buffer.length; i++) {
+            if (buffer[i] !== 0) {
+                newArray.push(buffer[i]);
+            }
+        }
+        return newArray;
+    }
+
+    function audioDetectBeat(bufferFrames, sensitivity) {
+        var averages = [],
+            averageChanges = [],
+            i, i2, bufferTotal, averageChange;
+
+        for (i = 0; i < bufferFrames.length; i++) {
+            bufferTotal = 0;
+            for (i2 = 0; i2 < bufferFrames[i].length; i2++) {
+                bufferTotal += bufferFrames[i][i2];
+            }
+
+            averages.push(bufferTotal / bufferFrames[i].length);
+        }
+
+        for (i = 0; i < averages.length - 1; i++) {
+            averageChanges.push(Math.abs(averages[i] - averages + 1));
+        }
+
+        for (i = 0; i < averageChanges.length; i++) {
+            averageChange += averageChanges[i];
+        }
+
+        averageChange = averageChange / averageChanges.length;
+
+        console.clear();
+        console.log(averageChange);
+    }
+
     function Visualizer(config) {
         var me = this,
             stage = new PIXI.Stage(0x000000),
             $canvas = config.canvas ? $(config.canvas)[0] : $('canvas')[0],
-            renderer = PIXI.autoDetectRenderer($canvas.width, 540, $canvas),
+            renderer = PIXI.autoDetectRenderer(1472, 736, $canvas),
             graphics = new PIXI.Graphics(renderer.view),
-            frequencyBuffer = config.frequencyBuffer || [],
-            activeVisualizer = config.activeVisualizer || 'bars';
+            activeVisualizer = config.activeVisualizer || 'bars',
+            audioContext = new webkitAudioContext(),
+            audioSource = audioContext.createMediaElementSource($('.trivisualizer-audio')[0]),
+            audioAnalyser = audioContext.createAnalyser();
+
+        audioAnalyser.fftSize = 256;
+
+        audioSource.connect(audioAnalyser);
+        audioAnalyser.connect(audioContext.destination);
 
         me.renderTarget = $canvas;
 
@@ -35,12 +92,11 @@
             setInterval(render, 16);
         };
 
-        me.listen = function (buffer) {
-            frequencyBuffer = buffer;
-        };
-
         function render() {
-            $.fn.trivisualizer.defaults.visualizers[activeVisualizer](frequencyBuffer, graphics, $canvas);
+            var frequencyData = new Uint8Array(audioAnalyser.frequencyBinCount);
+            audioAnalyser.getByteFrequencyData(frequencyData);
+
+            $.fn.trivisualizer.defaults.visualizers[activeVisualizer](bufferToArray(frequencyData), graphics, $canvas);
             renderer.render(stage);
         }
     }
@@ -65,17 +121,54 @@
                     canvasHeight = canvas.height,
                     numBuckets = buffer.length,
                     barRegion = (canvasWidth / numBuckets),
-                    barBaseline = canvasHeight / 2,
+                    barBaseline = canvasHeight * .7,
                     barWidth = barRegion * .8,
                     barOffset = barRegion / 2,
                     barX;
 
                 g.clear();
-                g.lineStyle(barWidth, 0x33CC33, 1);
+                g.lineStyle(barWidth, 0xFF6600, 1);
                 for (var i = 0; i < buffer.length; i++) {
                     barX = barOffset + barRegion * i;
                     g.moveTo(barX, barBaseline);
                     g.lineTo(barX, barBaseline - buffer[i]);
+                }
+                g.lineStyle(barWidth, 0xCC0000, 0.8);
+                for (var i = 0; i < buffer.length; i++) {
+                    barX = barOffset + barRegion * i;
+                    g.moveTo(barX, barBaseline - buffer[i]);
+                    g.lineTo(barX, barBaseline - buffer[i] - (buffer[i] * .1));
+                }
+                g.lineStyle(barWidth, 0xFF6600, 0.3);
+                for (i = 0; i < buffer.length; i++) {
+                    barX = barOffset + barRegion * i;
+                    g.moveTo(barX, barBaseline + 20);
+                    g.lineTo(barX, barBaseline + 20 + buffer[i] / 2);
+                }
+            },
+            blueDream: function (buffer, g, canvas) {
+                var canvasWidth = canvas.width,
+                    canvasHeight = canvas.height,
+                    numBuckets = buffer.length,
+                    buffer = audioClipEmpty(buffer),
+                    origin = {x: canvasWidth / 2, y: canvasHeight / 2},
+                    centerOffset = canvasHeight * 0.05,
+                    innerOffset = canvasHeight * 0.02,
+                    edgePadding = canvasHeight * 0.02,
+                    maxRadius = (canvasHeight / 2) - (centerOffset + edgePadding),
+                    radianSeparation = (2 * Math.PI) / numBuckets,
+                    barWidth = 512 / numBuckets,
+                    barX, barY;
+
+                g.clear();
+                g.lineStyle(barWidth, 0x0099FF, 1);
+                for (var i = 0; i < buffer.length; i++) {
+                    barX = (centerOffset * Math.cos(radianSeparation * i)) + origin.x;
+                    barY = (centerOffset * Math.sin(radianSeparation * i)) + origin.y;
+                    endX = ((centerOffset + buffer[i]) * Math.cos(radianSeparation * i)) + origin.x;
+                    endY = ((centerOffset + buffer[i]) * Math.sin(radianSeparation * i)) + origin.y;
+                    g.moveTo(barX, barY);
+                    g.lineTo(endX, endY);
                 }
             }
         }
